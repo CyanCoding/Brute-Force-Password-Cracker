8179055b1d5dbd7424704e0883ae24a0784c781d
Vishal Naik <59202862+Monsieurvishal@users.noreply.github.com>
2020-06-30 16:17:46 +0530

    update

--- Python/main.py
+++ Python/main.py
@@ -6,15 +6,19 @@ import time
 # Brute force function
 def tryPassword(passwordSet, stringTypeSet):
     start = time.time()
+    f = open("password_combination.txt", "w")  #It store all combinations ,It will easy be to analyse the pattern at the end
     chars = stringTypeSet
     attempts = 0
     for i in range(1, 9):
         for letter in itertools.product(chars, repeat=i):
             attempts += 1
             letter = ''.join(letter)
+            f.write(letter)
+            f.write("   ")
             if letter == passwordSet:
                 end = time.time()
                 distance = end - start
+                f.close()
                 return (attempts, distance)
 
 
