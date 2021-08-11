a158aeb151e2d4741796f52346a973eeffeccbce
Camden <camden.macleod@pm.me>
2021-08-11 19:15:08 -0400

    Add greeting

--- Go/main.go
+++ Go/main.go
@@ -1,5 +1,19 @@
+// Part of the "Brute Force Password Cracker" project at
+// https://github.com/CyanCoding/Brute-Force-Password-Cracker
+// Author: Camden Mac Leod, aka CyanCoding
+// File created on Aug 11, 2021
+
 package main
 
+import "fmt"
+
 func main() {
+	fmt.Println("Welcome to the Go Brute Force Password Cracker!")
+	fmt.Println("This program was created by CyanCoding.")
+	fmt.Println("-----------------------------------------------")
+	fmt.Print("Please enter your password > ")
+
+	var password = ""
+	fmt.Scanln(&password)
 
 }
